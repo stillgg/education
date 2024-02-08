06bae138dc4f3a49470c25a4130f29066ffb6017
Egor <still2095@gmail.com>
2024-02-08 20:23:10 +0300

    https://github.com/stillgg/education/issues/59
    add some methods

--- programming/lectures/TimurShemsedinov/Основы программирования с примерами на JavaScript (1 часть)/32/linkedList/class.js
+++ programming/lectures/TimurShemsedinov/Основы программирования с примерами на JavaScript (1 часть)/32/linkedList/class.js
@@ -1,76 +1,117 @@
-Node: {
-  //   class Node {
-  //     constructor(prev, value) {
-  //       const last = {
-  //         value,
-  //         prev,
-  //         next: null,
-  //       };
-  //       this.prev = last.prev;
-  //       this.next = last.next;
-  //       this.value = last.value;
-  //       if (this.prev !== null) {
-  //         Object.assign(this.prev, {
-  //           next: last,
-  //         });
-  //       }
-  //     }
-  //   }
-  //   const n1 = new Node(null, "first");
-  //   const n2 = new Node(n1, "second");
-}
+class LinkedList {
+  constructor() {
+    this.tail = null;
+    this.head = null;
+  }
 
-LinkedList: {
-  class LinkedList {
-    constructor() {
-      this.tail = null;
+  insert(index, data) {
+    const node = this.findNode((item, i) => index === i);
+
+    const prev = node.prev;
+    const next = node;
+
+    const insertElement = {
+      next,
+      prev,
+      value: data,
+    };
+
+    if (index === 0) this.head = insertElement;
+    if (prev !== null) prev.next = insertElement;
+    if (next !== null) next.prev = insertElement;
+  }
+
+  findNode(callback) {
+    let tail = this.head;
+
+    let index = 0;
+
+    while (callback(tail.value, index) === false) {
+      tail = tail.next;
+      index++;
+    }
+
+    return tail;
+  }
+
+  find(callback) {
+    const node = this.findNode(callback);
+    return node?.value;
+  }
+
+  delete(index, count) {
+    const start = this.findNode((item, i) => index === i);
+    const end = this.findNode((item, i) => index + count === i);
+
+    const isFirstDeleted = start.prev === null;
+    const isLastDeleted = end.next === null;
+
+    if (isFirstDeleted && isLastDeleted) {
       this.head = null;
+      this.tail = null;
+
+      return null;
+    } else if (isFirstDeleted) {
+      end.prev = this.tail;
+      this.tail.next = end;
+    } else if (isLastDeleted) {
+      start.next = this.tail;
+      this.tail.prev = start;
     }
 
-    push(value) {
-      const lastNode = {
-        prev: this.tail || null,
-        value,
-        next: null,
-      };
-
-      if (this.tail === null) this.head = lastNode;
-
-      this.tail = lastNode;
-
-      if (this.tail.prev !== null) {
-        Object.assign(this.tail.prev, {
-          next: lastNode,
-        });
-      }
-
-      return lastNode;
-    }
-
-    [Symbol.iterator]() {
-      let tail = this.head;
-      return {
-        next() {
-          if (tail === null) return { done: true };
-
-          const value = tail.value;
-          tail = tail.next;
-
-          return { done: false, value };
-        },
-      };
-    }
+    return this.head;
   }
 
-  const linkedList = new LinkedList();
+  push(value) {
+    const lastNode = {
+      prev: this.tail || null,
+      value,
+      next: null,
+    };
 
-  linkedList.push({ foo: "1" });
-  linkedList.push({ bar: "2" });
-  linkedList.push({ baz: "3" });
+    if (this.tail === null) this.head = lastNode;
 
-  // console.dir(linkedList.tail, { depth: 5 });
+    this.tail = lastNode;
 
-  for (const item of linkedList) {
-    console.log("item - ", item);
+    if (this.tail.prev !== null) {
+      Object.assign(this.tail.prev, {
+        next: lastNode,
+      });
+    }
+
+    return lastNode;
+  }
+
+  [Symbol.iterator]() {
+    let tail = this.head;
+    return {
+      next() {
+        if (tail === null) return { done: true };
+
+        const value = tail.value;
+        tail = tail.next;
+
+        return { done: false, value };
+      },
+    };
   }
 }
+
+const linkedList = new LinkedList();
+
+linkedList.push({ foo: "1" });
+linkedList.push({ bar: "2" });
+linkedList.push({ baz: "4" });
+
+linkedList.insert(2, { baz: "3" });
+linkedList.insert(0, { foo: "0" });
+
+// console.log(linkedList.find((item, index) => index === 0));
+
+console.log(linkedList.delete(1, 1));
+
+// console.dir(linkedList.tail, { depth: 5 });
+
+// for (const item of linkedList) {
+//   console.log("item - ", item);
+// }
