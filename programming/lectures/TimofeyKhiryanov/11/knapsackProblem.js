b35de9f983fd75e381ad7bb00f578f560ceb553d
Egor <still2095@gmail.com>
2024-05-01 13:35:57 +0300

    https://github.com/stillgg/education/issues/11

--- programming/lectures/TimofeyKhiryanov/11/knapsackProblem.js
+++ programming/lectures/TimofeyKhiryanov/11/knapsackProblem.js
@@ -1,31 +1,49 @@
-const solution = (elements) => {
+const EMPTY = { name: "", cost: 0, weight: 0 };
+
+const max = (...objects) => {
+  let max = { ...EMPTY };
+
+  for (let i = 0; i < objects.length; i++) {
+    const obj = objects[i];
+    if (obj.cost > max.cost) max = obj;
+  }
+
+  return max;
+};
+
+const solution = (elements, maxWeight) => {
   const weights = elements.map((i) => i.weight);
-  const maxWeight = Math.max(...weights);
+  maxWeight = maxWeight || Math.max(...weights);
   const minWeight = Math.min(...weights);
 
   const lenEl = elements.length;
   const len = Math.round(maxWeight / minWeight);
   const matrix = new Array(lenEl + 1)
     .fill(0)
-    .map((i) => new Array(len + 1).fill(0));
+    .map((i) => new Array(len + 1).fill(0).map((i) => ({ ...EMPTY })));
 
   for (let i = 1; i < lenEl + 1; i++) {
-    let n = 0;
     for (let j = 1; j < len + 1; j++) {
       const el = elements[i - 1];
       const left = matrix[i][j - 1];
       const top = matrix[i - 1][j];
       const diagonal = matrix[i - 1][j - 1];
 
-      const weight = (j - n) * minWeight;
+      const weight = j * minWeight;
 
       if (weight === el.weight) {
-        matrix[i][j] = Math.max(el.cost, top, left);
-      } else if (weight > el.weight) {
-        matrix[i][j] = Math.max(diagonal + el.cost, top, left);
-        n = j;
+        matrix[i][j] = max(el, top, left);
+      } else if (weight >= diagonal.weight + el.weight) {
+        const obj = {
+          name:
+            diagonal.name === "" ? el.name : `${diagonal.name} + ${el.name}`,
+          cost: diagonal.cost + el.cost,
+          weight: diagonal.weight + el.weight,
+        };
+
+        matrix[i][j] = max(obj, top, left);
       } else {
-        matrix[i][j] = Math.max(top, left);
+        matrix[i][j] = max(top, left);
       }
     }
   }
@@ -42,14 +60,14 @@ const elements = [
     weight: 1,
   },
   {
-    name: "laptop",
-    cost: 2000,
-    weight: 3,
-  },
-  {
     name: "radio",
     cost: 3000,
     weight: 4,
+  },
+  {
+    name: "laptop",
+    cost: 2000,
+    weight: 3,
   },
   {
     name: "iphone",
@@ -68,4 +86,4 @@ const elements = [
   },
 ];
 
-console.log(solution(elements));
+console.log(solution(elements, 9));
