2584d998d9745d39f3c8da054e5671384e6e9c1d
Egor <still2095@gmail.com>
2024-05-08 13:34:57 +0300

    https://github.com/stillgg/education/issues/12 kmp

--- programming/lectures/TimofeyKhiryanov/12/prefixFunction/DynamicProgramming.js
+++ programming/lectures/TimofeyKhiryanov/12/prefixFunction/DynamicProgramming.js
@@ -1,21 +1,24 @@
 const solution = (s) => {
-  const arr = new Array(s.length).fill(0);
+  const p = new Array(s.length).fill(0);
 
   for (let i = 1; i < s.length; i++) {
-    let j = arr[i - 1];
-    while (j > 0 && s[i] !== s[j]) {
-      j = arr[j - 1];
+    let j = p[i - 1];
+
+    while (j > 0 && s[j] !== s[i]) {
+      j = p[j - 1];
     }
+
     if (s[i] === s[j]) j++;
-    arr[i] = j;
+
+    p[i] = j;
   }
 
-  return arr;
+  return p;
 };
 
 // let s = "abcabcd";
 // let s = "abab";
 // let s = "aa";
-let s = "aaxaxaaaax";
+// let s = "aaxaxaaaax";
 
 console.log(solution(s));
