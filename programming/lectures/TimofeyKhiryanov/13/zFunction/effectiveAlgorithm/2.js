0b0cbff33700a7ef3ae2315ab984e33e62d8a4a8
Egor <still2095@gmail.com>
2024-05-18 15:10:25 +0300

    https://github.com/stillgg/education/issues/13

--- programming/lectures/TimofeyKhiryanov/13/zFunction/effectiveAlgorithm/2.js
+++ programming/lectures/TimofeyKhiryanov/13/zFunction/effectiveAlgorithm/2.js
@@ -1,34 +1,21 @@
 const solution = (s) => {
-  const len = s.length;
-  const z = new Array(len).fill(0);
-
-  let left = 0;
-  let right = 0;
-  let i = 1;
-
-  while (i < len) {
-    if (i > right) {
-      // out of the box
-      right = i;
-
-      while (s[right - i] === s[right]) {
-        right++;
-      }
-
-      z[i] = right - 1 - left;
-      left++;
-    } else {
-      // in the box. We've already calculated all the values
-
-      z[i] = z[i - left];
+  const n = s.length;
+  const z = new Array(n).fill(0);
+  let l = 0;
+  let r = 0;
+  for (let i = 1; i < n; i++) {
+    if (i < r) {
+      z[i] = Math.min(r - i, z[i - l]);
     }
-
-    i++;
+    while (i + z[i] < n && s[z[i]] === s[i + z[i]]) {
+      z[i]++;
+    }
+    if (i + z[i] > r) {
+      l = i;
+      r = i + z[i];
+    }
   }
-
   return z;
 };
 
-console.log(solution("aaaa"));
-// console.log(solution("abab"));
-// console.log(solution("aabxaabxcaabxaabxay"));
+console.log(solution("aabxaabxcaabxaabxay"));
