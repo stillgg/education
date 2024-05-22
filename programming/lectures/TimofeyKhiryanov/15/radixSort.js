ba41c2adc7c9a3247c693f1e76c60c9c7fd2cc20
Egor <still2095@gmail.com>
2024-05-22 17:32:41 +0300

    https://github.com/stillgg/education/issues/15

--- programming/lectures/TimofeyKhiryanov/15/radixSort.js
+++ programming/lectures/TimofeyKhiryanov/15/radixSort.js
@@ -1,5 +1,15 @@
-const solution = () => {};
+
+
+const solution = (arr) => {
+  const max = Math.max(arr);
+  for (let i = 10; i < max; i *= 10) {}
+
+  for(let i=0;i<arr.length; i++){
+    arr[i]
+  }
+};
 
 const arr1 = [170, 45, 75, 90, 802, 24, 2, 66];
 
+console.log(solution(arr1));
 // 170 90 802 2 24 45 75 66
